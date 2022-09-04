aceaea41495418aeb528d180b9bbe507cc158c68
JOBIRBEK ABDURAHMANOV <wwwjobirchik7@mail.ru>
2022-09-04 23:09:49 +0900

    birinchi-dars

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className='container-in'>
+    
+
+            <div className='conta-acadamy'>
+              <div className='conta-p'>
+                <div className='conta-right'>
+                  <div className='conta-right-text'>
+              
+                    <p>Kurslar</p>
+                    <p>ochiq darslar</p>
+                    <p>Biz bilan aloqa</p>
+                  
+             
+                  </div>
+            
+           
+                    
+                </div>
+              </div>
+              
+   
+             </div>
+      
+              </div>
+            </div>
+            </>
+
+       
+
+
+
+
+
+  )
+}
+
+export default App
---
+++ src/testing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css';
+
+const Testing = () => {
+  return (
+    <div>
+    <div className='container'>
+        bbb
+    </div>
+    </div>
+  )
+}
+
+export default Testing
